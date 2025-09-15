f8d538d970dbb9abb1eec4652b7c1bb1e331943f
jkleparchuk <jkleparchuk@gmail.com>
2025-09-15 13:50:10 -0700

    Add files via upload

---
+++ 405hw1b.m
@@ -0,0 +1,76 @@
+% part b
+
+a = 2;
+Tf = 0.3;
+h_values = [1/4, 1/8, 1/16, 1/32, 1/64, 1/128, 1/256];
+errors = zeros(size(h_values));
+
+for i = 1:length(h_values)
+    h = h_values(i);
+
+    x = 0:h:(1-h);
+    x = x';
+    N = length(x);
+
+    % IC
+    u0 = sin(a*pi*x);
+
+    % Matrix L
+    e = ones(size(x));
+    L = spdiags([e -2*e e], [-1 0 1], N, N);
+    L(1, N) = 1;  % periodic boundary conditions
+    L(N, 1) = 1;
+    L = (1/h^2) * L;
+
+    k = 0.4*h^2;
+    numsteps = ceil(Tf / k);
+    k = Tf / numsteps;
+
+    % time loop
+    u = u0;
+    for n=1:numsteps
+        % (unew - u) / k = u_xx
+        % solve for unew and approx u_xx
+        unew = u + k*(L*u);
+        u = unew;
+    end
+
+    u_exact = sin(a*pi*x) * exp(-(a*pi)^2 * Tf);
+    errors(i) = max(abs(u - u_exact));
+end
+
+% table of results
+fprintf('h\t\tError\t\tError Ratio\n');
+fprintf('--------------------------------\n');
+fprintf('%f\t%e\t', h_values(1), errors(1));
+fprintf('-\n');
+for i = 2:length(h_values)
+    ratio = errors(i-1) / errors(i);
+    fprintf('%f\t%e\t%.2f\n', h_values(i), errors(i), ratio);
+end
+
+% part c
+figure(1);
+loglog(h_values, errors, 'bo-');
+set(gca, 'fontsize', 18)
+hold on;
+
+% best fit line
+log_h = log10(h_values);
+log_error = log10(errors);
+
+p = polyfit(log_h, log_error, 1);
+slope = p(1);
+intercept = p(2);
+
+h_fit = logspace(log10(h_values(1)), log10(h_values(end)), 100);
+error_fit = 10.^(intercept + slope*log10(h_fit));
+
+loglog(h_fit, error_fit, 'r-');
+
+% style
+xlabel('h', 'FontSize', 18);
+ylabel('Max Error at t=0.3', 'FontSize', 18);
+legend('Computed Error', sprintf('Best Fit (slope = %.2f)', slope));
+grid on;
+
