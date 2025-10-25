e3106831c8e67a57cd5348cec5c1421f0e2e8c17
jkleparchuk <jkleparchuk@gmail.com>
2025-10-24 19:32:43 -0700

    Add files via upload

---
+++ hw3/q6a final.m
@@ -0,0 +1,26 @@
+format long g
+
+error = [];
+hs = [];
+for n = [10, 20, 40, 80, 160, 320]
+    h = 1/(n+1);
+    x = (h:h:1-h)';
+    f = (-pi^2 - 1) * sin(pi * x);
+    uexact = sin(pi * x);
+
+    main = -2 * ones(n,1);
+    off = ones(n-1,1);
+    L = (diag(main) + diag(off,1) + diag(off,-1)) / h^2;
+    A = L - eye(n);
+
+    unumeric = A \ f;
+    error(end+1) = max(abs(unumeric - uexact));
+    hs(end+1) = h;
+end
+
+loglog(hs, error, 'o-');
+xlabel('h', 'fontsize', 18);
+ylabel('Max Error', 'fontsize', 18);
+title('Error vs h', 'fontsize', 18);
+grid on;
+
